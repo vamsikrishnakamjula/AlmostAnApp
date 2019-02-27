f278bebbaed027428e2284a7beb34e82f791dfde
Vamsikvkr <vamsikvkr@Vamsikvkrs-MacBook-Pro.local>
2019-02-26 23:11:35 -0800

    Added Framework Files

---
+++ AlmostAnApp/AlmostAnApp/AlmostAnApp.h
@@ -0,0 +1,19 @@
+//
+//  AlmostAnApp.h
+//  AlmostAnApp
+//
+//  Created by Vamsikvkr on 2/26/19.
+//  Copyright © 2019 almostanapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlmostAnApp.
+FOUNDATION_EXPORT double AlmostAnAppVersionNumber;
+
+//! Project version string for AlmostAnApp.
+FOUNDATION_EXPORT const unsigned char AlmostAnAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlmostAnApp/PublicHeader.h>
+
+
